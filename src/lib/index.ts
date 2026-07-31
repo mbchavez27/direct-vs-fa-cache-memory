c15c623e7eb2b9743cf608b3b4e1f08cd3afc81b
jrsantiagoo <jrsantiago3001@gmail.com>
2026-07-31 22:50:38 +0800

    feat: implement compareCaches()

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,18 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+// ONLY FOR TESTING... TO BE DELETED LATER
+import { CacheSimulator } from "./simulator/CacheSimulator";
+
+const cacheSimulator = new CacheSimulator("Fully-Associative-MRU", {
+    blockSizeWords: 2,
+    cacheBlockCount: 4,
+    mainMemoryBlockCount: 1024,
+    readPolicy: "non-load-through",
+    cacheAccessTimeNs: 1,
+    memoryAccessTimeNs: 10,
+});
+
+cacheSimulator.loadSequence([1, 17, 2, 1023, 3, 19, 1, 1]);
+cacheSimulator.runToEnd();
+console.log(cacheSimulator.getCurrentSnapshot());
+console.log(cacheSimulator.getSimulationResult());
--- src/lib/simulator/compareCaches.ts
+++ src/lib/simulator/compareCaches.ts
@@ -1,10 +1,23 @@
-import type { CacheConfig, ComparisonResult } from '../cache/types';
-import { CacheSimulator } from './CacheSimulator';
+import type { CacheConfig, ComparisonResult } from "../cache/types";
+import { CacheSimulator } from "./CacheSimulator";
 
-export function compareCaches(config: CacheConfig, sequence: number[]): ComparisonResult {
-    // TODO: Run simulation for both Direct-Mapped and Fully Associative MRU caches
-    // using the exact same sequence.
-    
-    // Return dummy data for now
-    return {} as ComparisonResult;
+// Runs both the Direct-mapped & Fully Associative cache simulators on the same sequence
+export function compareCaches(
+    config: CacheConfig,
+    sequence: number[],
+): ComparisonResult {
+    const directSimulator = new CacheSimulator("Direct-Mapped", config);
+    directSimulator.loadSequence(sequence);
+    directSimulator.runToEnd();
+
+    const fsaSimulator = new CacheSimulator("Fully-Associative-MRU", config);
+    fsaSimulator.loadSequence(sequence);
+    fsaSimulator.runToEnd();
+
+    const comparisonResult: ComparisonResult = {
+        directMapped: directSimulator.getSimulationResult(),
+        fullyAssociative: fsaSimulator.getSimulationResult(),
+    };
+
+    return comparisonResult;
 }
