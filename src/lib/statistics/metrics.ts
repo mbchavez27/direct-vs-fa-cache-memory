03b369b4568863892f7d21d28400f25f9856d512
jrsantiagoo <jrsantiago3001@gmail.com>
2026-07-31 12:04:39 +0800

    feat: complete metrics

--- src/lib/statistics/metrics.ts
+++ src/lib/statistics/metrics.ts
@@ -1,20 +1,34 @@
-import type { SimulationStatistics } from '../cache/types';
+import type { SimulationStatistics } from "../cache/types";
 
 export function calculateMetrics(
     totalAccesses: number,
     hits: number,
     misses: number,
-    totalMemoryAccessTimeNs: number
+    cacheAccessTimeNs: number,
+    memoryAccessTimeNs: number,
+    totalMemoryAccessTimeNs: number,
 ): SimulationStatistics {
     // TODO: Calculate hitRate, missRate, and averageMemoryAccessTimeNs
-    
+
+    let hitRate = 0;
+    let missRate = 0;
+    // Prevent undefined results
+    if (totalAccesses !== 0) {
+        hitRate = hits / totalAccesses;
+        missRate = misses / totalAccesses;
+    }
+
+    // T_avg = hC + (1-h)*M
+    let averageMemoryAccessTimeNs =
+        hitRate * cacheAccessTimeNs + missRate * memoryAccessTimeNs;
+
     return {
         totalAccesses,
         hits,
         misses,
-        hitRate: 0,
-        missRate: 0,
-        averageMemoryAccessTimeNs: 0,
-        totalMemoryAccessTimeNs
+        hitRate,
+        missRate,
+        averageMemoryAccessTimeNs,
+        totalMemoryAccessTimeNs,
     };
 }
